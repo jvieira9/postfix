f1ca3c43cd1ea8a50b5095fdbe489f39307439e3
João Vieira <joao.lopes.vieira115@gmail.com>
2023-11-30 12:30:31 -0100

    Base

---
+++ conf/config.local.php
@@ -0,0 +1,15 @@
+<?php
+		$CONF['database_type'] = 'mysqli';
+		$CONF['database_host'] = 'db';
+		$CONF['database_port'] = '3306';
+		$CONF['database_user'] = 'postfixadmin';
+		$CONF['database_password'] = 'postfixadmin';
+		$CONF['database_name'] = 'postfixadmin';
+		$CONF['setup_password'] = 'changeme';
+		$CONF['smtp_server'] = 'localhost';
+		$CONF['smtp_port'] = '25';
+		$CONF['encrypt'] = 'md5crypt';
+		$CONF['configured'] = true;
+		$CONF['dkim'] = 'NO';
+		$CONF['dkim_all_admins'] = 'NO';
+		?>
